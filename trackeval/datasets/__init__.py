471695dc8964bf4677d623ea23f3587136d657a1
gmaaliki <maalikighifari@gmail.com>
2025-05-05 17:24:01 +0700

    update numpy version

--- trackeval/datasets/__init__.py
+++ trackeval/datasets/__init__.py
@@ -15,3 +15,4 @@ from .youtube_vis import YouTubeVIS
 from .head_tracking_challenge import HeadTrackingChallenge
 from .rob_mots import RobMOTS
 from .person_path_22 import PersonPath22
+from .soccernet_mot_challenge_2d_box import SoccernetMotChallenge2DBox
